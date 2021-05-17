7f7426edd57280a452b916d41a8c0e423f737812
Carolina Oliveira <carolinasao6@gmail.com>
2021-05-17 20:52:32 -0300

    Finalizada as rotas

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from komercio import json_products
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/products')
 def list_products():
+    page = request.args.get('page')
+    per_page = request.args.get('per_page')
+
     return jsonify(json_products)
 
-# @app.route('/products/<product_id')
-# def get_product():
-#     pass
+@app.route('/products/<product_id>')
+def get_product(product_id):
+    for product in json_products:
+        if product['id'] == int(product_id):
+            return product
