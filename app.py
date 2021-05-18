c86eb1be5cdd1da768c38498d7fbb21d27e4e103
Carolina Oliveira <carolinasao6@gmail.com>
2021-05-17 22:10:04 -0300

    tentativa de paginacao

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ CORS(app)
 
 @app.route('/products')
 def list_products():
-    page = request.args.get('page')
+    page = request.args.get('page', default=1, type=int)
     per_page = request.args.get('per_page')
+    # pagenation = json_products.query.paginate(page=page, per_page=per_page)
 
-    return jsonify(json_products)
+    return jsonify(json_products))
 
 @app.route('/products/<product_id>')
 def get_product(product_id):
